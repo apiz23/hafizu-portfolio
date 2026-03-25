cd9cea3572150297822bbb80e44074194c101861
Hafizu <piz230601@gmail.com>
2026-03-25 12:54:07 +0800

    again

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -1,5 +1,24 @@
-import supabase from "@/lib/supabase";
 import { NextRequest, NextResponse } from "next/server";
+import supabase from "@/lib/supabase";
+
+export async function GET(
+	req: NextRequest,
+	context: { params: Promise<{ id: string }> },
+) {
+	const { id } = await context.params;
+
+	const { data, error } = await supabase
+		.from("projects")
+		.select("*")
+		.eq("id", id)
+		.single();
+
+	if (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 });
+	}
+
+	return NextResponse.json(data);
+}
 
 export async function DELETE(
 	req: NextRequest,
