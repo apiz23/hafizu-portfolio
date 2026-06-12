b2aaa8428c1afa7f2ba0878c7971136dc5fcb735
Hafizu <piz230601@gmail.com>
2026-06-12 13:04:54 +0800

    feat: wire Skills section into page between Intro and Education

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,19 @@ import Experience from "@/components/Experience";
 import Education from "@/components/Education";
 import Contact from "@/components/Contact";
 import Intro from "@/components/Intro";
+import Skills from "@/components/Skills";
 
 export default function Home() {
 	return (
 		<main>
 			<div className="relative z-0">
 				<Intro />
+				<Skills />
 				<Education />
 				<Experience />
 				<Project />
 				<Contact />
 			</div>
-
-			{/* Optional: Sticky Cursor (uncomment if you want it) */}
-			{/* <StickyCursor /> */}
 		</main>
 	);
 }
